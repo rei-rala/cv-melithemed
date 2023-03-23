fe950eb04797ab38ab75f14fe121fe782d63189c
Ramon Irala <rirala@Garantizar.com.ar>
2023-03-23 17:33:25 -0300

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/reset.css'
-import type { AppProps } from 'next/app'
+import "../styles/reset.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
